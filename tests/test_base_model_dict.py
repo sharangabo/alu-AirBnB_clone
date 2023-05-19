fffa912b469c8edd5325db8bf44da8c7a9abcdcf
Fese Mbile <109585199+fesembile@users.noreply.github.com>
2023-05-19 12:20:38 +0000

    .

--- tests/test_base_model_dict.py
+++ tests/test_base_model_dict.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""Defines FileStorage class."""
-
-
 from models.base_model import BaseModel
 
 my_model = BaseModel()
@@ -17,3 +14,12 @@ print("JSON of my_model:")
 for key in my_model_json.keys():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
                                    my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
